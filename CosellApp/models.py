a4b6e1c72b4b779577cb4b6f1e8b4db43733afea
Tushar25022001 <tushar25022001@gmail.com>
2021-09-26 22:08:01 +0530

    Product Model Added

--- CosellApp/models.py
+++ CosellApp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from datetime import datetime
-
+from django.utils import timezone
 
 class Student(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -20,3 +20,15 @@ class Payment(models.Model):
 
     def __str__(self):
         return self.transection_id
+
+class Product(models.Model):
+    Id= models.AutoField(primary_key=True) #  Not Using default id provided by Django
+    Name = models.CharField(max_length=100)
+    Description = models.TextField(blank=False) #Description is must
+    Price =  models.DecimalField(max_digits=10,decimal_places=2)
+    DateTime=models.DateTimeField(default=timezone.now) #Date once added cannot be changed
+    SellerInfo = models.ForeignKey(Student, on_delete=models.CASCADE)
+    Photo = models.ImageField(default='NotsetDefaultPicture.jpg',upload_to='profile_pics')
+
+    def __str__(self):
+        return f'Id is {self.Id} Name is {self.Name}'
