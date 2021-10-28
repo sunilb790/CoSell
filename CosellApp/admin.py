fe9f645f2d86b87297e4c0e6ea8c7b95103c6f51
shubham vora <shubhamvora05@gmail.com>
2021-10-28 17:52:37 +0530

    chat implementation

--- CosellApp/admin.py
+++ CosellApp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Student, Payment, Product, FAQ, SellerBuyer,Profile
+from .models import Student, Payment, Product, FAQ, SellerBuyer, Profile, Message
 
 admin.site.register(Student)
 admin.site.register(Payment)
@@ -7,3 +7,4 @@ admin.site.register(Product)
 admin.site.register(FAQ)
 admin.site.register(SellerBuyer)
 admin.site.register(Profile)
+admin.site.register(Message)
